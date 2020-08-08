627555565b6a5162fee67999fcd1a05e20dc32ac
mhkmcp <mhkmcp@yahoo.com>
2020-08-08 17:38:11 +0600

    Added Comment System

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -59,4 +59,21 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day
                              )
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        print(comment_form)
+        if comment_form.is_valid():
+            # create comment object bu don't save it yet
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form
+                   })
--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -115,8 +115,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your.email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your.password'
+EMAIL_HOST_USER = 'your@email'
+EMAIL_HOST_PASSWORD = 'your@password'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
