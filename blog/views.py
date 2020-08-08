e59191da7efb7669f4fba3761c16a85c74f72cc3
mhkmcp <mhkmcp@yahoo.com>
2020-08-08 20:53:53 +0600

    Added Suggestion of Posts by Similarity

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
+from django.db.models import Count
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
@@ -56,7 +57,9 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post_list.html', {'page': page, 'posts': posts})
+
+    return render(request, 'blog/post_list.html',
+                  {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -78,9 +81,14 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post_detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts
                    })
